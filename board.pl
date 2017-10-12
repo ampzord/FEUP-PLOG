b8249e4a4cd6a7933ebf446486dce532b6ab09d9
Huguinho <huguinho27@users.noreply.github.com>
2017-10-12 15:14:35 +0100

    english version

--- board.pl
+++ board.pl
@@ -7,7 +7,7 @@
 /*white tower - wt */
 /*black tower - bt */
 
-campo([
+board([
 ['  ','  ','  ','  ','  ','  ','  ','  '],
 ['  ','  ','  ','  ','  ','  ','  ','  '],
 ['  ','  ','  ','  ','  ','  ','  ','  '],
@@ -21,33 +21,36 @@ campo([
 %listaBrancas([r,c,b,t]).
 %listaPretas([rr,cc,bb,tt]).
 
-/*----impressao do campo de jogo*/
+printBoard(Board):-write('  1  2  3  4  5  6  7  8  '), nl, printField(Board, 1), write('  A  B  C  D  E  F  G  H '), nl, nl, !.
 
-/*imprimirTabuleiro(lista correspondente ao campo)*/
-imprimirTabuleiro(Campo):-write('  1  2  3  4  5  6  7  8  '), nl, imprimirCampo(Campo, 1), write('  A  B  C  D  E  F  G  H '), nl, nl, !.
+printField(_,9).
+printField(Board,Line):-write(Line), 
+						printLine(Board,Line,1), 
+						write(' '), write(Line), 
+						nl, 
+						LineAux is Line + 1, 
+						printField(Board,LineAux).
 
-imprimirCampo(_,9).
-imprimirCampo(Campo,Linha):-write(Linha), imprimirLinha(Campo,Linha,1), write(' '), write(Linha), nl, LinhaAux is Linha + 1, imprimirCampo(Campo,LinhaAux).
+printLine(_,_,9).
+printLine(Board,Line,Column):- write(' '), 
+								getPiece(Board,Line/Column,Piece), 
+								write(Piece), 
+								ColumnAux is Column + 1, 
+								printLine(Board, Line,ColumnAux).
 
-imprimirLinha(_,_,9).
-imprimirLinha(Campo,Linha,Coluna):- write(' '), 
-									obterPeca(Campo,Linha/Coluna,Peca), 
-									write(Peca), 
-									ColunaAux is Coluna + 1, 
-									imprimirLinha(Campo, Linha,ColunaAux).
+getPiece(Board,Line/Column,Piece):-elementInPosition(Line,Board,L,1), 
+									elementInPosition(Column,L,Piece,1).
 
-obterPeca(Campo,Linha/Coluna,Peca):-elementoNaPosicao(Linha,Campo,L,1), elementoNaPosicao(Coluna,L,Peca,1).
-
-elementoNaPosicao(Posicao,[X|_],X,Posicao).
-elementoNaPosicao(Posicao,[_|L],Peca,Cont) :- Cont1 is Cont+1, elementoNaPosicao(Posicao,L,Peca,Cont1).
+elementInPosition(Position,[X|_],X,Position).
+elementInPosition(Position,[_|L],Piece,Cont) :- Cont1 is Cont+1, elementInPosition(Position,L,Piece,Cont1).
 
 
-jogar(Xi-Yi/Xf-Yf,Campo):-write(Xi).
+jogar(Xi-Yi/Xf-Yf,Board):-write(Xi).
 
 /*jogadaValida
-jogadaValida(Xi-Yi/Xf-Yf,Campo):-dentroCampo(Xi-Yi/Xf-Yf,Campo),verificarCor(Xi-Yi/Xf-Yf),verificarPeca(Xi-Yi/Xf-Yf).
+jogadaValida(Xi-Yi/Xf-Yf,Board):-dentroCampo(Xi-Yi/Xf-Yf,Board),verificarCor(Xi-Yi/Xf-Yf),verificarPiece(Xi-Yi/Xf-Yf).
 
 verificarCor(Xi-Yi/Xf-Yf):-eBranca(Xi-Yi),verificarAtacarPreta().
 verificarCor(Xi-Yi/Xf-Yf):-ePreta(Xi-Yi),verificarAtacarBranca().
 
-verificarPeca(Xi-Yi/Xf-Yf) :- retornarPeca(Xi-Yi,Peca), podeAtacar(Xi-Yi,Peca,Xf-Yf).
+verificarPiece(Xi-Yi/Xf-Yf) :- retornarPiece(Xi-Yi,Piece), podeAtacar(Xi-Yi,Piece,Xf-Yf).

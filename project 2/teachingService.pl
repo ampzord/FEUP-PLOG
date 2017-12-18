3ae685067a194674786342b486107c0f6aacaf4b
António Pereira <antonio_m_sp@hotmail.com>
2017-12-18 17:56:52 +0000

    asda

--- project 2/teachingService.pl
+++ project 2/teachingService.pl
@@ -28,38 +28,28 @@ professores de cada tipo e áreas científicas, bem como as preferências dos pr
 */
 
 /*
-
 uma unidade curricular(UC) tem horas teoricas (HT) e horas praticas (HP), (HP > HT).
 UCs tem areas cientificas
 
 
 Aulas teoricas atribuidas -> profs da area científica
-aulas praticas atribuidas -> profs da area cientifica (preferencialmente)
+Aulas praticas atribuidas -> profs da area cientifica (preferencialmente)
 
 Profs -> tem areas cientificas
-Profs -> catedraticos, auxiliares e associados (tipo)
-Profs -> carga horaria entre 7h e 9h semanais na media dos 2 semestres ( a diferença da carga horaria entre os 2 semestres e indicado na pref do Prof)
+Profs -> catedraticos, auxiliares e associados (tipo) 7h, 8h e 9h , respectivamente (media entre os 2 semestres) ( a diferença da carga horaria entre os 2 semestres e indicado na pref do Prof)
 
 Variaveis a variar :
 
 numero de UCs
 horas a leccionar
-numero de profs de cada tipo e area cientifica
-preferencias dos profs
+numero Profs
 
 MUDA-SE NA BASE DE DADOS EZ
 */
 
-
-teachingService(N_CurricularUnits, N_LectureHours, N_Professors) :-
+servicoDocente(N_UnidadesCurriculares, N_Professores) :-
 	clearScreen,
-	welcomeMessage,
-
+	nl, write('----- Distribuicao de Servico Docente -----'), nl, nl.
 	%criar listas
 
 	%impor dominios
-
-
-% Welcome Message
-welcomeMessage :-
-	nl, write('----- Teaching Service Distribution -----'), nl, nl.
